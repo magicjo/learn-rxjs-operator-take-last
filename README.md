5bad5bd5f914460fab67bbf6d2ec29ee5b265728
Johan GIRARD <magicjo@tiglab.net>
2019-06-22 00:00:30 +0200

    Improve example

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+[![Build Status](https://travis-ci.org/magicjo/learn-rxjs-operator-take-last.svg?branch=master)](https://travis-ci.org/magicjo/learn-rxjs-operator-take-last)
+
 # learn-rxjs-operator-take-last
 
+Example about the filtering operator `takeLast`.
+
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/learn-rxjs-operator-take-last)
+
+## How to execute in local ?
+
+Execute `src/index.ts`:
+```bash
+docker run -v $(pwd):/app/src magicjo/learn-rxjs:latest start
+```
+
+Execute `tslint`:
+```bash
+docker run -v $(pwd):/app/src magicjo/learn-rxjs:latest lint
+```
--- index.ts
+++ index.ts
@@ -1,9 +1,29 @@
-import { of } from 'rxjs'; 
-import { map } from 'rxjs/operators';
+// RxJS v6+
+import { interval } from "rxjs";
+import { takeLast, takeWhile } from "rxjs/operators";
 
+/*
+ Filtering Operator: takeLast
+ ----------------------------
 
-const source = of('World').pipe(
-  map(x => `Hello ${x}!`)
-);
+ Remembers the latest **count** values, then emits those only when the source completes.
+ ```
+ takeLast(count: number): Observable
+ ```
 
-source.subscribe(x => console.log(x));
+ http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-takeLast
+*/
+
+// Emit: 0, 1, 2, ..., 20 (every 0.20s)
+const source = interval(200).pipe(takeWhile(n => n <= 20));
+
+// Take the last 3 emitted values
+const example = source.pipe(takeLast(3));
+
+// Receive: 20, 19, 18
+console.log("[start]");
+example.subscribe({
+  complete: () => console.log("[complete]"),
+  error: err => console.error("[error] : ", err),
+  next: value => console.log("[next] : ", value)
+});
